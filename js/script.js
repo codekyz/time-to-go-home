c6e6ed15e73326b6890aa56bc068f43a3b057e43
codekyz <codekyz0111@gmail.com>
2022-01-23 10:27:44 +0900

    :art: Update to ES6
    Update to ES6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const startButton = document.querySelector('.start_btn');
-const shareButton1 = document.querySelector('.share_btn1');
-const shareButton2 = document.querySelector('.share_btn2');
+const shareButton = document.querySelector('.share_btn');
 const closeButton = document.querySelector('.close_btn');
 
 const modal = document.querySelector('.modal_wrap');
@@ -13,18 +12,17 @@ const currentTime = document.querySelector('.current_time');
 const gohomeTime = document.querySelector('.gohome_time');
 
 // 1초마다 현재 시각 갱신
-function showClock() {
+const showClock = () => {
     getClock();
     setInterval(getClock, 1000);
 }
 
 // 1초마다 남은 시간 갱신
-function startTimer() {
-    setInterval(calculator, 1000);
-}
+const startTimer = () => setInterval(calculator, 1000);
+
 
 // 현재 시각을 출력하는 함수
-function getClock() {
+const getClock = () => {
     let date1 = new Date();
     let hours = date1.getHours();
     let minutes = date1.getMinutes();
@@ -34,7 +32,7 @@ function getClock() {
 }
 
 // 퇴근 시간 - 현재 시간으로 남은 시간을 계산하는 함수
-function calculator() {
+const calculator = () => {
     let date2 = new Date;
     let date3 = new Date;
     let userTime = date3.setHours(Number(userHour.value), Number(userMinute.value), 0, 0);
@@ -63,11 +61,11 @@ function calculator() {
     }
 }
 
-function openModal() {
+const openModal = () => {
     if(calculator()) {
         loading.style.display = 'flex';
         document.querySelector('#modal h2').innerHTML = `퇴근까지`;
-        setTimeout(function () {
+        setTimeout(() => {
             loading.style.display = 'none';
             modal.style.display = 'flex';
         }, 2000);
@@ -75,20 +73,18 @@ function openModal() {
     }
 }
 
-function closeModal() {
+const closeModal = () => {
     modal.style.display = 'none';
     window.location.reload();
 }
 
-window.onclick = function (event) {
-    if(event.target == modal) {
-        closeModal();
-    }
+window.onclick = (event) => { 
+    event.target === modal ? closeModal() : '';
 }
 
 showClock();
 
-function copyTime1() {
+const copyTime = () => {
     let tmp = document.createElement('input');
 
     document.body.appendChild(tmp);
@@ -101,22 +97,9 @@ function copyTime1() {
     alert('퇴근시간이 복사되었습니다.')
 }
     
-function copyTime2() {
-    let tmp = document.createElement('input');
-
-    document.body.appendChild(tmp);
-    tmp.value = '저 퇴근까지 '+gohomeTime.innerText+' 남았습니다.';
-    tmp.select();
-    document.execCommand("copy");
-    document.body.removeChild(tmp);
-
-    alert('퇴근시간이 복사되었습니다.')
-
-}
 
 
 startButton.addEventListener('click', openModal);
-shareButton1.addEventListener('click', copyTime1);
-shareButton2.addEventListener('click', copyTime2);
+shareButton.addEventListener('click', copyTime);
 closeButton.addEventListener('click', closeModal);
 
